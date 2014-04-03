c0c692f7b8dfe909318da6a237a9f711835dea35
francis.chan <f1ancis621@gmail.com>
2014-04-03 09:52:33 +0800

    add auto r define support. add text toggle animation.

--- src/js/graph.js
+++ src/js/graph.js
@@ -1,48 +1,75 @@
 nod.graph = function () {
   'use strict';
 
-  var margin = {top: 40, bottom: 20, left: 80, right: 20}
+  var margin = {top: 0, bottom: 0, left: 0, right: 0}
     , directed = false
     , height = 1600
     , width = 900
     , colors = d3.scale.category20()
     , radius = 15
+    , maxRadius = 50
+    , deg = []
   ;
 
-  function preprocess (data) {
-    // var res = [];
-    // for (var i in data) {
-    // }
-
-    return data;
+  function preprocess (graph) {
+    for (var i in graph.nodes) {
+      graph.nodes[i].links = [];
+    }
+    for (var i in graph.links) {
+      var link = graph.links[i];
+      graph.nodes[link.source].links.push(i);
+      graph.nodes[link.target].links.push(i);
+    }
+    for (var i in graph.nodes) {
+      deg[i] = graph.nodes[i].links.length;
+    }
+    return graph;
   }
 
   function chart (selection) {
-  	selection.each(function (data) {
-      var d = preprocess(data);
+  	selection.each(function (graph) {
+      var d = preprocess(graph);
+
+      var r = d3.scale.linear()
+          .domain(d3.extent(deg))
+          .range([1, maxRadius])
 
       var force = d3.layout.force()
-          .nodes(data.nodes)
-          .links(data.links)
+          .nodes(graph.nodes)
+          .links(graph.links)
           .linkDistance(200)
-          .size([800, 600])
-          .charge(-100)
+          .size([width, height])
+          .charge(-200)
           .on("tick", tick)
           .start();
 
-      var marker = d3.select(this).append('defs').selectAll('.marker')
-          .data(['killed', 'attempted'])
-        .enter().append("marker")
-          .attr('id', function(d) { return d; })
-          .attr('refX', radius+10)
-          .attr('refY', 5)
-          .attr('markerWidth', 15)
-          .attr('markerHeight', 15)
-          .attr('orient', 'auto')
-        .append('path')
-          .attr('d', 'M0,0L10,5L0,10L3,5')
+      // var marker = d3.select(this).append('defs').selectAll('.marker')
+      //     .data(['killed', 'attempted'])
+      //   .enter().append("marker")
+      //     .attr('id', function(d) { return d; })
+      //     .attr('refX', radius+10)
+      //     .attr('refY', 5)
+      //     .attr('markerWidth', 15)
+      //     .attr('markerHeight', 15)
+      //     .attr('orient', 'auto')
+      //   .append('path')
+      //     .attr('d', 'M0,0L10,5L0,10L3,5')
+
+      // Not showing up
+      // var icon = d3.select(this).select('defs').selectAll('.icon')
+      //     .data(force.nodes())
+      //   .enter().append('pattern')
+      //     .attr('id', function (d) { return d.idty; })
+      //     .attr('patternUnits', 'userSpaceOnUse')
+      //     .append('image')
+      //     .attr('xlink:href', function(d) { return d.img })
+      //     .attr('x', 0)
+      //     .attr('y', 0)
+      //     .attr('width', radius)
+      //     .attr('height', radius)
 
       var plot = d3.select(this).append('g')
+          .attr('class', 'graph')
           .attr('transform', function (d, i) { return 'translate(' + margin.left + ',' + margin.top + ')'; });
    
       var link = plot.selectAll(".link")
@@ -50,25 +77,30 @@ nod.graph = function () {
         .enter().append("line")
           .attr("class", "link")
           .style("stroke-width", function (d) { return Math.sqrt(d.value); })
-          .attr("marker-end", function (d) { return "url(#" + d.type + ")"; })
+          // .attr("marker-end", function (d) { return "url(#" + d.type + ")"; })
 
       var node = plot.selectAll(".node")
           .data(force.nodes())
         .enter().append("circle")
           .style('stroke', function (d, i) { return colors(d.color); })
+          .style('stroke-width', 2)
           .style('fill', 'white')
-          .attr("r", radius)
-          .attr('id', function (d) { return 'node-' + d.idty; })
+          .attr("r", function (d) { return r(d.links.length); })
+          .attr('id', function (d, i) { return 'node-' + i; })
           .attr('class', 'node')
+          .attr('data-idty', function (d) { return d.idty;})
           .call(force.drag);
 
-      var text = plot.selectAll(".text")
+      var text = plot.selectAll("text")
           .data(force.nodes())
-        .enter().append("text")
-          .attr("x", 8)
+        .enter().append('a')
+          .attr("xlink:href", function(d) { return d.url; })
+          .append("text")
+          .attr("x", function (d) { return r(d.links.length)+2;})
           .attr("y", ".31em")
-          .attr('class', 'text hide')
-          .attr('id', function (d) { return 'text-' + d.idty; })
+          .style('display', 'none')
+          .attr('id', function (d, i) { return 'text-' + i; })
+          .attr('data-idty', function (d) { return d.idty;})
           .text(function(d) { return d.name; });
 
       function tick() {
@@ -78,14 +110,38 @@ nod.graph = function () {
             .attr("y1", function(d) { return d.source.y; })
             .attr("x2", function(d) { return d.target.x; })
             .attr("y2", function(d) { return d.target.y; });
+
+        force.stop();
+        setTimeout(function () {
+          force.start();
+        }, 100);
       }
 
       function transform(d) {
         return "translate(" + d.x + "," + d.y + ")";
       }
 
+      // animation
+      var that = this;
+      $(this).on('mouseenter', '.node', function (e) {
+        // hide all
+        $(that).find('text').hide();
+        $(that).find('circle')
+          .css('fill', 'white');
+
+        var idx = $(this).attr('id').split('-')[1];
+        var node = graph.nodes[idx];
+        for (var _ in node.links) {
+          var i = node.links[_];
+          $('#text-'+graph.links[i].target.index).show();
+          $('#node-'+graph.links[i].target.index)
+            .css('fill', 'gray')
+
+          $('#text-'+graph.links[i].source.index).show();
+        }
+      })
   	});
-    
+
     return chart;
   }
 
@@ -100,6 +156,12 @@ nod.graph = function () {
 		height = _;
 		return chart;
 	}
+
+  chart.maxRadius = function (_) {
+    if (!arguments.length) return maxRadius;
+    maxRadius = _;
+    return chart;
+  }
 
   return chart;
 };
